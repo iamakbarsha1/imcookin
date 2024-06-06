fd3afe06952e46f0b4fef85f1c5d66444e086219
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-06-06 19:44:26 +0530

    utils_api: Add /oauth, /login route - axios

--- src/api/auth.js
+++ src/api/auth.js
@@ -9,11 +9,23 @@ export const ax_OAuth_googleSingin = async (data) => {
     }
     return null;
   } catch (e) {
-    console.log("e.response.data", e.response.data);
-    if (e.response.status === 404) {
-      return e.response.data;
-    } else {
-      return null;
+    console.log("e.response.data -> ", e.response.data);
+    if (e.response.status === 404) return e.response.data;
+    else return null;
+  }
+};
+
+export const ax_login = async (data) => {
+  try {
+    const res = await APICall.post("/auth/login", data);
+    if (res.status === 200 || res.status === 201) {
+      console.log("login route: -> " + res);
+      return res.data;
     }
+    return null;
+  } catch (e) {
+    console.log("e.response.data -> ", e.response.data);
+    if (e.response.status === 404) return e.response.data;
+    else return null;
   }
 };
