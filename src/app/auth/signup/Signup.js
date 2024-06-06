b2e9cc601f73a56aadb248b7c4baa20f2980707b
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-06-06 19:42:42 +0530

    feat: Add signup page - working

---
+++ src/app/auth/signup/Signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+import FormPage from "./section/FormPage";
+const Login = () => {
+  return (
+    <main className="h-screen w-full flex items-center justify-center">
+      <FormPage />
+    </main>
+  );
+};
+
+export default Login;
