45378026094c9ed6f8ab160929213bca2f1beb7d
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-06-11 19:32:52 +0530

    feat: Add Signup UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./app/auth/login/Login";
+import Signup from "./app/auth/signup/Signup";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/auth" element={<Login />} />
-        {/* <Route path="/auth/signup" element={<Login />} /> */}
+        <Route path="/auth/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
