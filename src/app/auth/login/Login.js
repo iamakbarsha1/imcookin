d1bf12b368a88f5de5b5aa619c366c6d9b63e647
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-06-11 19:32:13 +0530

    feat: Reword the UI

--- src/app/auth/login/Login.js
+++ src/app/auth/login/Login.js
@@ -2,8 +2,10 @@ import React from "react";
 import FormPage from "./section/FormPage";
 const Login = () => {
   return (
-    <main className="h-screen w-full flex items-center justify-center">
-      <FormPage />
+    <main className="h-screen max-w-6xl mx-auto">
+      <section className="h-full w-full flex items-center justify-center">
+        <FormPage />
+      </section>
     </main>
   );
 };
