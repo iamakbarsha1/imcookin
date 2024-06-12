2d384ef6317c2d31394f111e7ee32953c0c68be8
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-06-13 00:08:20 +0530

    feat: Add Signup UI, Add links in login - Singup UI, Add dynamic form UI in Signup UI

--- src/app/auth/signup/Signup.js
+++ src/app/auth/signup/Signup.js
@@ -5,7 +5,47 @@ import SignupNav from "../../../components/SignupNav";
 const Signup = () => {
   const [signupData, setSignupData] = useState({
     email: "",
+    password: "",
+    name: "",
+    username: "",
   });
+
+  const formArr = [
+    {
+      label: "Enter your email address",
+      name: "email",
+      value: signupData.email,
+      isComplete: false,
+      isNext: false,
+      isMandatory: true,
+    },
+    {
+      label: "Create a password",
+      name: "password",
+      value: signupData.password,
+      isComplete: false,
+      isNext: false,
+      isMandatory: true,
+    },
+    {
+      label: "Enter your name",
+      name: "name",
+      value: signupData.name,
+      isComplete: false,
+      isNext: false,
+      isMandatory: true,
+    },
+    {
+      label: "Enter username",
+      name: "username",
+      value: signupData.username,
+      isComplete: false,
+      isNext: false,
+      isMandatory: true,
+    },
+  ];
+
+  // const sendQuery =
 
   const onHandleChange = (e) => {
     const { name, value } = e.target;
@@ -28,19 +68,29 @@ const Signup = () => {
             <div className="text-base tracking-wider text-stone-400">
               Let's begin the adventure
             </div>
-            <form className="mt-5 flex items-end">
-              <section className="w-full">
-                <InputTag
-                  label={"Enter your email address*"}
-                  name={"email"}
-                  type={"text"}
-                  value={signupData?.email}
-                  onChange={(e) => onHandleChange(e)}
-                />
-              </section>
-              <button className="py-1 px-3 ml-2 text-sm border border-borderWhite bg-secondaryDark rounded-md shadow-md">
-                Continue
-              </button>
+            <form className="mt-5">
+              {/* {JSON.stringify(signupData)} */}
+              {formArr.map((input) => {
+                return (
+                  <main className="h-0 flex items-end">
+                    <section className="mt-3 w-full">
+                      <InputTag
+                        label={input.label}
+                        name={input.name}
+                        type={"text"}
+                        value={input?.value}
+                        // isMandatory={}
+                        onChange={(e) => onHandleChange(e)}
+                      />
+                    </section>
+                    <button
+                      className={` py-1 px-3 ml-2 text-sm border border-borderWhite bg-secondaryDark rounded-md shadow-md`}
+                    >
+                      Continue
+                    </button>
+                  </main>
+                );
+              })}
             </form>
           </section>
         </main>
