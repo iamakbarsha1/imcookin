c752d68e2ebc8c7fce3d660cf33a8e81e7510899
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-06-13 00:06:32 +0530

    utils: Add debounce script for API calling

---
+++ src/utils/debounce.js
@@ -0,0 +1,11 @@
+exports.debounce = (func, wait) => {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      timeout = null;
+      func.apply(context, args);
+    }, wait);
+  };
+};
