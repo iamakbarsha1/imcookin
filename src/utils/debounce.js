1733eb450df79a2380297a5d75bdd4e866248694
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-07-15 23:37:24 +0530

    utils: update/comment debounce fn.

--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -1,11 +1,19 @@
-exports.debounce = (func, wait) => {
+// exports.debounce = (func, wait) => {
+//   let timeout;
+//   return function (...args) {
+//     const context = this;
+//     if (timeout) clearTimeout(timeout);
+//     timeout = setTimeout(() => {
+//       timeout = null;
+//       func.apply(context, args);
+//     }, wait);
+//   };
+// };
+
+exports.debounce = (func, delay) => {
   let timeout;
-  return function (...args) {
-    const context = this;
-    if (timeout) clearTimeout(timeout);
-    timeout = setTimeout(() => {
-      timeout = null;
-      func.apply(context, args);
-    }, wait);
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), delay);
   };
 };
